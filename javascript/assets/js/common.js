a92e09f62dc7c06f9b9865129837707dca32682d
kimm99604 <109261995+mj0614k@users.noreply.github.com>
2022-08-25 19:10:09 +0900

    220825

---
+++ javascript/assets/js/common.js
@@ -0,0 +1,33 @@
+hljs.highlightAll();
+
+// 모달
+const modalBtn = document.querySelector(".modal__btn");
+const modalClose = document.querySelector(".modal__close")
+const modalCont = document.querySelector(".modal__cont");
+
+modalBtn.addEventListener("click", () => {
+    modalCont.classList.add("show");
+    modalCont.classList.remove("hide");
+})
+modalClose.addEventListener("click", () => {
+    modalCont.classList.add("hide");
+})
+
+// 탭 메뉴
+const tabBtn = document.querySelectorAll(".modal__box .tabs > div");
+const tabCont = document.querySelectorAll(".modal__box .cont > div");
+
+tabBtn.forEach((e, i) => {
+    e.addEventListener("click", (event) => {
+        event.preventDefault();  // 버튼을 클릭해도 스크롤 제자리
+
+        tabBtn.forEach(e => {
+            e.classList.remove("active")
+        })
+        e.classList.add("active")
+        tabCont.forEach(div => {
+            div.style.display = "none";
+        })
+        tabCont[i].style.display = "block";
+    });
+});
