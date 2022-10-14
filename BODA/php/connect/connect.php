fddf21c5331ce8bc27b1a0615131a20db34b14a7
jwor12427 <109261995+mj0614k@users.noreply.github.com>
2022-10-14 15:08:17 +0900

    221014

--- BODA/php/connect/connect.php
+++ BODA/php/connect/connect.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     $host = "localhost";
     $user = "mo0647";
@@ -6,13 +5,4 @@
     $db = "mo0647";
     $connect = new mysqli($host, $user, $pass, $db);
     $connect -> set_charset("utf8");
-=======
-<?php
-    $host = "localhost";
-    $user = "mo0647";
-    $pass = "sjsksk79513!";
-    $db = "mo0647";
-    $connect = new mysqli($host, $user, $pass, $db);
-    $connect -> set_charset("utf8");
->>>>>>> d0f3fa85d8f698a2ea05ec58967ac59d04712ee2
 ?>
--- BODA/php/connect/session.php
+++ BODA/php/connect/session.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 <?php 
     session_start();
-=======
-<?php 
-    session_start();
->>>>>>> d0f3fa85d8f698a2ea05ec58967ac59d04712ee2
 ?>
