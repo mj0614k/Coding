288e3581c7c1c59654112e00aa5937e71b63f039
jwor12427 <109261995+mj0614k@users.noreply.github.com>
2022-10-14 15:28:53 +0900

    221014

--- BODA/php/create/createMember.php
+++ BODA/php/create/createMember.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     include "../connect/connect.php";
 
@@ -16,25 +15,4 @@
     $sql .= ")charset=utf8;";
 
     $connect -> query($sql);
-
-=======
-<?php
-    include "../connect/connect.php";
-
-    $sql = "CREATE TABLE myMember (";
-    $sql .= "myMemberID int(10) unsigned NOT NULL auto_increment,";
-    $sql .= "youID varchar(40) NOT NULL,";
-    $sql .= "youName varchar(10) NOT NULL,";
-    $sql .= "youPass varchar(40) NOT NULL,";
-    $sql .= "youNickName varchar(10) NOT NULL,";
-    $sql .= "youEmail varchar(40) NOT NULL,";
-    $sql .= "youBirth varchar(20) NOT NULL,";
-    $sql .= "youPhone varchar(20) NOT NULL,";
-    $sql .= "regTime int(20) NOT NULL,";
-    $sql .= "PRIMARY KEY (myMemberID)";
-    $sql .= ")charset=utf8;";
-
-    $connect -> query($sql);
-
->>>>>>> d0f3fa85d8f698a2ea05ec58967ac59d04712ee2
 ?>
